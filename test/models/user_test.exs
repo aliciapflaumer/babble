cf7abedbb39387d1b6d991ed5bcd514f6c4fb366
aliciapflaumer <alpfla7@gmail.com>
2018-02-07 13:41:22 -0500

    Create model for authentication
    - To create migrations, used
    `mix phoenix.gen.model User users email:unique encrypt_pass:string`
    
    - Checked migration in priv/repo/migrations
    
    - In models/user file, added line to schema `users`:
    `field :password, :string, virtual: true`,
    and under `changeset` changed `encrypt_pass` to `password`
    
    - Ran in terminal `mix ecto.migrate`

---
+++ test/models/user_test.exs
@@ -0,0 +1,18 @@
+defmodule Babble.UserTest do
+  use Babble.ModelCase
+
+  alias Babble.User
+
+  @valid_attrs %{email: "some email", encrypt_pass: "some encrypt_pass"}
+  @invalid_attrs %{}
+
+  test "changeset with valid attributes" do
+    changeset = User.changeset(%User{}, @valid_attrs)
+    assert changeset.valid?
+  end
+
+  test "changeset with invalid attributes" do
+    changeset = User.changeset(%User{}, @invalid_attrs)
+    refute changeset.valid?
+  end
+end
