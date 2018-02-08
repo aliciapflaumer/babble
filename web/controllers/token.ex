b6f59502359da0a10afd836d7e9851cffd5c8edf
aliciapflaumer <alpfla7@gmail.com>
2018-02-08 14:57:32 -0500

    Created sign-in on index route
    - Created sign in that points to new /chat route

---
+++ web/controllers/token.ex
@@ -0,0 +1,16 @@
+
+defmodule Babble.Token do
+	use Babble.Web, :controller
+
+	def unauthenticated(conn, _params) do
+		conn
+		|> put_flash(:error, "You must be signed in!")
+		|> redirect(to: session_path(conn, :new))
+	end
+
+	def unauthorized(conn, _params) do
+	    conn
+	    |> put_flash(:error, "You must be signed in!")
+	    |> redirect(to: session_path(conn, :new))
+  	end
+end
--- web/models/user.ex
+++ web/models/user.ex
@@ -18,4 +18,23 @@ defmodule Babble.User do
     |> validate_required([:email, :password])
     |> unique_constraint(:email)
   end
+
+  def reg_changeset(struct, params \\ %{}) do
+    struct
+    |> changeset(params)
+    |> cast(params, [:password], [])
+    |> validate_length(:password, min: 5)
+    |> hash_pw()
+  end
+
+  defp hash_pw(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: p}} ->
+        put_change(changeset, :encrypt_pass, Comeonin.Pbkdf2.hashpwsalt(p))
+
+      _ ->
+        changeset
+    end
+  end
+  
 end
