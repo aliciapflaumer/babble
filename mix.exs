6fba5000960d2be38ccdd3d9f43d1c46ad4ad89d
aliciapflaumer <alpfla7@gmail.com>
2018-03-20 16:20:20 -0400

    Add changes

--- mix.exs
+++ mix.exs
@@ -20,12 +20,6 @@ defmodule Babble.Mixfile do
   def application do
     [
       mod: {Babble, []},
-<<<<<<< HEAD
-
-      extra_applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :comeonin, :ueberauth, :httpoison, :postgrex]
-
-=======
->>>>>>> Add changes
       extra_applications: [:logger, :comeonin, :ueberauth]
     ]
   end
