feb747028f009ee780127981bc64bb538458dea3
aliciapflaumer <alpfla7@gmail.com>
2018-03-20 13:08:05 -0400

    Edit to prod file

--- config/prod.exs
+++ config/prod.exs
@@ -28,6 +28,7 @@ config :babble, Babble.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
 
+
 # ## SSL Support
 #
 # To get SSL working, you will need to add the `https` key
